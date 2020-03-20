b3ea2acb334b330541e07bc2e513ecec58c3ab66
760644586 <41118619+760644586@users.noreply.github.com>
2020-03-20 15:50:37 +0800

    Update Aosp9.sh

--- Aosp9.sh
+++ Aosp9.sh
@@ -1,6 +1,16 @@
-sudo apt-get -y install openjdk-8-jdk
-sudo apt-get -y install python3.7
-sudo apt-get -y install python3-pip
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y python3.7
+sudo apt-get install -y python3-pip
+sudo apt-get install -y libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib
+sudo apt-get install -y git flex bison gperf build-essential libncurses5-dev:i386
+sudo apt-get install -y tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386
+sudo apt-get install -y dpkg-dev libsdl1.2-dev libesd0-dev
+sudo apt-get install -y git-core gnupg flex bison gperf build-essential  
+sudo apt-get install -y zip curl zlib1g-dev gcc-multilib g++-multilib
+sudo apt-get install -y libc6-dev-i386
+sudo apt-get install -y lib32ncurses5-dev x11proto-core-dev libx11-dev
+sudo apt-get install -y libgl1-mesa-dev libxml2-utils xsltproc unzip m4
+sudo apt-get install -y lib32z-dev ccache
 
 mkdir ~/bin
 PATH=~/bin:$PATH
