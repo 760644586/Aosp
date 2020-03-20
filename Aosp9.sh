fa945e2c54128f690994ddf980f49bdfd9210bab
760644586 <41118619+760644586@users.noreply.github.com>
2020-03-20 16:35:17 +0800

    Update Aosp9.sh

--- Aosp9.sh
+++ Aosp9.sh
@@ -1,24 +1,18 @@
 mkdir ~/bin
 PATH=~/bin:$PATH
-timeout 3
 
 rm -fr ./git-repo
-timeout 3
+
 
 git clone https://gerrit-googlesource.lug.ustc.edu.cn/git-repo
-timeout 3
 
 sudo cp -r ./git-repo/. ./bin
-timeout 3
 chmod a+x ~/bin/repo
-timeout 3
 
 git config --global user.email "xiaomi@xiaomi.com"
 git config --global user.name "xiaomi"
 
-mkdir -p Android9
-timeout 3
-cd Android9
-timeout 3
+mkdir -p ~/Android9
+cd ~/Android9
 
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 9.x
