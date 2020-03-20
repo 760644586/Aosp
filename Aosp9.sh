96d1c6f67864b0b9bb7b6e2ecc562ffafac8246e
760644586 <41118619+760644586@users.noreply.github.com>
2020-03-20 16:43:30 +0800

    Update Aosp9.sh

--- Aosp9.sh
+++ Aosp9.sh
@@ -12,7 +12,6 @@ chmod a+x ~/bin/repo
 git config --global user.email "xiaomi@xiaomi.com"
 git config --global user.name "xiaomi"
 
-mkdir -p ~/Android9
 cd ~/Android9
 
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 9.x
